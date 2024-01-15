a92ad8d3cd369b5859fdac3f1496aa9ba84ca561
HighBoys <hamdanimuhhammad106@gmail.com>
2024-01-15 14:47:40 +0700

    menambahkan validasi request

--- src/route/bangunDatar.ts
+++ src/route/bangunDatar.ts
@@ -1,5 +1,9 @@
 import  express  from "express"
 import { kelilingLingkaran, kelilingPersegi, kelilingPersegiPanjang, luasLingkaran, luasPersegi, luasPersegiPanjang, luasSegitiga } from "../controler/bangunDatar"
+import { validatelingkaran } from "../middleware/validateLingkaran"
+import { validatepersegi } from "../middleware/validatePersegi"
+import { validatepersegipanjang } from "../middleware/validatePersegiPanjang"
+import { validatesegitiga } from "../middleware/validateSegitiga"
 const app = express()
 
 /** allow read a body */
@@ -9,12 +13,12 @@ app.use(express.json())
  * sebuah fungsi pada object express.
  *  fungsi tsb akan otomatis  dijalankan */
 
-app.post(`/lingkaran/luas` ,luasLingkaran)
-app.post(`/lingkaran/keliling` ,kelilingLingkaran)
-app.post(`/persegi/luas` ,luasPersegi)
-app.post(`/persegi/keliling` ,kelilingPersegi)
-app.post(`/persegipanjang/luas` ,luasPersegiPanjang)
-app.post(`/persegipanjang/luas` ,kelilingPersegiPanjang)
-app.post(`/segitiga/luas` ,luasSegitiga)
+app.post(`/lingkaran/luas`,validatelingkaran, luasLingkaran,)
+app.post(`/lingkaran/keliling` , validatelingkaran,kelilingLingkaran)
+app.post(`/persegi/luas` ,validatepersegi,luasPersegi)
+app.post(`/persegi/keliling` ,validatepersegi,kelilingPersegi)
+app.post(`/persegipanjang/luas` ,validatepersegipanjang,luasPersegiPanjang)
+app.post(`/persegipanjang/luas` ,validatepersegipanjang,kelilingPersegiPanjang)
+app.post(`/segitiga/luas` ,validatesegitiga,luasSegitiga)
 
 export default app
--- src/route/bangunRuang.ts
+++ src/route/bangunRuang.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import { luaspermukaanBalok, luaspermukaanBola, luaspermukaanKubus, luaspermukaanTabung, volumeBalok, volumeBola, volumeTabung, volumekubus } from "../controler/bangunRuang";
+import { validatetabung } from "../middleware/ValidateTabung";
+import { validatekubus } from "../middleware/validateKubus";
+import { validatebalok } from "../middleware/validateBalok";
+import { validatebola } from "../middleware/validateBola";
 const app = express()
 
 
 /** use digunakan untuk memberi izin  */
 app.use(express.json())
 
-app.post(`/tabung/volume` ,volumeTabung)
-app.post(`/tabung/luaspermukaan` ,luaspermukaanTabung)
-app.post(`/kubus/volume` ,volumekubus)
-app.post(`/kubus/luaspermukaan` ,luaspermukaanKubus)
-app.post(`/balok/volume` ,volumeBalok)
-app.post(`/balok/luaspermukaan` ,luaspermukaanBalok)
-app.post(`/bola/volume` ,volumeBola)
-app.post(`/bola/luaspermukaan` ,luaspermukaanBola)
+app.post(`/tabung/volume` ,validatetabung,volumeTabung)
+app.post(`/tabung/luaspermukaan` ,validatetabung,luaspermukaanTabung)
+app.post(`/kubus/volume` ,validatekubus,volumekubus)
+app.post(`/kubus/luaspermukaan` ,validatekubus,luaspermukaanKubus)
+app.post(`/balok/volume` ,validatebalok,volumeBalok)
+app.post(`/balok/luaspermukaan` ,validatebalok,luaspermukaanBalok)
+app.post(`/bola/volume` ,validatebola,volumeBola)
+app.post(`/bola/luaspermukaan` ,validatebola,luaspermukaanBola)
 
 export default app
